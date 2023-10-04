f3b64cf1080af2b7fd3dd971d450049c4e47ad43
George A. Fratian <84881414+ElPsyKurisu@users.noreply.github.com>
2023-10-04 19:36:37 -0400

    added instrument and version change
    testing version change system, and adding a new instrument

---
+++ src/ekpy/control/instruments/digilent/core.py
@@ -0,0 +1,17 @@
+import mcculw
+import numpy as np
+from .. import misc
+from ....utils import get_number_and_suffix
+
+__all__ = ('function', 'config_channels_in', 'config_channels_out')
+
+def function():
+    #do something
+    return
+
+def config_channels_in():
+    return
+
+
+
+def config_channels_out
