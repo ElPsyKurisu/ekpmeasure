a9ee9f56838dd62173d8f8e0c9899b3200d3e2a8
George A. Fratian <84881414+ElPsyKurisu@users.noreply.github.com>
2023-10-04 19:51:29 -0400

    Update core.py

--- src/ekpy/control/instruments/digilent/core.py
+++ src/ekpy/control/instruments/digilent/core.py
@@ -1,4 +1,6 @@
-import mcculw
+from mcculw import ul
+from mcculw.enums import InterfaceType
+from mcculw.device_info import DaqDeviceInfo
 import numpy as np
 from .. import misc
 from ....utils import get_number_and_suffix
@@ -13,5 +15,73 @@ def config_channels_in():
     return
 
 
+def config_channels_out(use_device_detection=True, dev_id_list=[],
+                         board_num=0):
+    '''
+    Configures which channels will be active for output
+    '''   
+    # By default, the example detects and displays all available devices and
+    # selects the first device listed. Use the dev_id_list variable to filter
+    # detected devices by device ID (see UL documentation for device IDs).
+    # If use_device_detection is set to False, the board_num variable needs to
+    # match the desired board number configured with Instacal.
 
-def config_channels_out
+    try:
+        if use_device_detection:
+            config_first_detected_device(board_num, dev_id_list)
+
+        daq_dev_info = DaqDeviceInfo(board_num)
+        if not daq_dev_info.supports_analog_output:
+            raise Exception('Error: The DAQ device does not support '
+                            'analog output')
+
+        print('\nActive DAQ device: ', daq_dev_info.product_name, ' (',
+              daq_dev_info.unique_id, ')\n', sep='')
+
+        ao_info = daq_dev_info.get_ao_info()
+        ao_range = ao_info.supported_ranges[0]
+    
+    except Exception as e:
+        print('\n', e)
+    finally:
+        if use_device_detection:
+            ul.release_daq_device(board_num)
+
+
+
+def config_first_detected_device(board_num, dev_id_list=None):
+    """Adds the first available device to the UL.  If a types_list is specified,
+    the first available device in the types list will be add to the UL.
+
+    Parameters
+    ----------
+    board_num : int
+        The board number to assign to the board when configuring the device.
+
+    dev_id_list : list[int], optional
+        A list of product IDs used to filter the results. Default is None.
+        See UL documentation for device IDs.
+    """
+    ul.ignore_instacal()
+    devices = ul.get_daq_device_inventory(InterfaceType.ANY)
+    if not devices:
+        raise Exception('Error: No DAQ devices found')
+
+    print('Found', len(devices), 'DAQ device(s):')
+    for device in devices:
+        print('  ', device.product_name, ' (', device.unique_id, ') - ',
+              'Device ID = ', device.product_id, sep='')
+
+    device = devices[0]
+    if dev_id_list:
+        device = next((device for device in devices
+                       if device.product_id in dev_id_list), None)
+        if not device:
+            err_str = 'Error: No DAQ device found in device ID list: '
+            err_str += ','.join(str(dev_id) for dev_id in dev_id_list)
+            raise Exception(err_str)
+
+    # Add the first DAQ device to the UL with the specified board number
+    ul.create_daq_device(board_num, device)
+
+
