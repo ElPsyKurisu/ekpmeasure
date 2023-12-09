33c3d57205894a6bc173846d47691d469055f727
Geo Fratian <84881414+ElPsyKurisu@users.noreply.github.com>
2023-12-08 21:35:05 -0500

    Update core.py

--- src/ekpy/control/instruments/keysight81150a/core.py
+++ src/ekpy/control/instruments/keysight81150a/core.py
@@ -2,6 +2,7 @@
 This is for the KEYSIGHT 81150A Arbitrary Pulse Generator and requires the KEYSIGHT I/O Libraries to function
 '''
 import numpy as np
+from typing import Union
 
 __all__ = ('idn', 'reset', 'initialize', )
 
@@ -64,6 +65,52 @@ def configure_trigger(wavegen, channel: str='1', source: str='IMM', mode: str='E
     wavegen.write(":ARM:SENS{} {}".format(channel, mode))
     wavegen.write(":ARM:SLOP {}".format(slope))
 
-def create_arbitrary_waveform():
-    
+def create_arbitrary_waveform(wavegen, data, name: str='ARB1',):
+    """
+    This program creates an arbitrary waveform within the limitations of the
+    Keysight 81150A which has a limit of 2 - 524288 data points.
+    args:
+        wavegen (pyvisa.resources.gpib.GPIBInstrument): Keysight 81150A
+        channel (str): Desired Channel to configure accepted params are [1,2]
+        source (str): Trigger source allowed args = [IMM (immediate), INT2 (internal), EXT (external), MAN (software trigger)]
+        mode (str): The type of triggering allowed args = [EDGE (edge), LEV (level)]
+        slope (str): The slope of triggering allowed args = [POS (positive), NEG (negative), EIT (either)]
+    """  
+    #will want to include error handling in this one.
+
+
+#https://github.com/jeremyherbert/barbutils/blob/master/barbutils.py
+#upper frequnecy range is 120MHZ so do not go above that
+#maybe i can use this barb stuff to read the waveforms too...
+#finds avg max - min /2 can probably use githubn library to generate barb file then pass that
+
+def configure_arb_waveform(wavegen, channel: str='1', name='ARB1', gain: str='1.0', offset: str='0.00', freq: str='1000'):
+    """
+    This program configures arbitrary waveform already saved on the instrument. Taken from LabVIEW. 
+    args:
+        wavegen (pyvisa.resources.gpib.GPIBInstrument): Keysight 81150A
+        channel (str): Desired Channel to configure accepted params are [1,2]
+        name (str): The Arbitrary Waveform name as saved on the instrument
+        gain (str): The V_pp by which the waveform should be gained by
+        offset (str): The voltage offset in units of volts
+        freq (str): the frequency in units of Hz for the arbitrary waveform
+    """
+    wavegen.write(":FUNC{}:USER {}:FUNC{} USER".format(channel, name, channel))
+    wavegen.write(":VOLT{} {}".format(channel, gain))
+    wavegen.write(":FREQ{} {}".format(channel, freq))
+    wavegen.write(":VOLT{}:OFFS {}".format(channel, offset))  
+
+
+'''
+Helper functions:
+'''
+
+def scale_waveform_data(data: Union[np.array, list]) -> np.array:
+    '''
+    Scales the data between -1 and 1 then multiplies by instrument specific
+    scaling factor (8191 for ours)
+    '''
+    data = np.array(data)
+    normalized = 2*(data - np.min(data))/np.ptp(data) - 1
+    return normalized * 8191
 
